42dc151eb1fe18ec58dd8ab75c08f4ceb0e0e0b9
AlexB8675 <alexandr.benbaccar@gmail.com>
2021-03-16 22:55:05 +0100

    fixed fucked formatting lol

--- wss/main.go
+++ wss/main.go
@@ -4,32 +4,32 @@ import (
     "encoding/json"
     "github.com/gin-gonic/gin"
     "gopkg.in/olahol/melody.v1"
-	"time"
+    "time"
 )
 
 type Event struct {
-    Op      int    		`json:"op"`
-    Type 	string 		`json:"type"`
+    Op      int         `json:"op"`
+    Type    string      `json:"type"`
     Payload interface{} `json:"payload"`
 }
 
 type HelloEvent struct {
-	HeartbeatInterval int `json:"heartbeat_interval"`
+    HeartbeatInterval int `json:"heartbeat_interval"`
 }
 
 type MessageEvent struct {
-	Author  string `json:"author"`
-	Content string `json:"content"`
+    Author  string `json:"author"`
+    Content string `json:"content"`
 }
 
 func ignore_error(val interface{}, _ error) interface{} {
-	return val
+    return val
 }
 
 func main() {
-	sessions := make(map[*melody.Session]int64)
-    server   := gin.Default()
-    wss      := melody.New()
+    sessions := make(map[*melody.Session]int64)
+    server := gin.Default()
+    wss := melody.New()
 
     server.GET("/", func(context *gin.Context) {
         if wss.HandleRequest(context.Writer, context.Request) != nil {
@@ -38,20 +38,20 @@ func main() {
     })
 
     wss.HandleConnect(func(s *melody.Session) {
-		_ = s.Write(ignore_error(json.Marshal(Event{10, "", HelloEvent{60000}})).([]byte))
-		sessions[s] = time.Now().UnixNano() / int64(time.Millisecond)
-	})
+        _ = s.Write(ignore_error(json.Marshal(Event{10, "", HelloEvent{60000}})).([]byte))
+        sessions[s] = time.Now().UnixNano() / int64(time.Millisecond)
+    })
 
     wss.HandleMessage(func(s *melody.Session, msg []byte) {
-		var event Event
-		_ = json.Unmarshal(msg, &event)
-		switch event.Type {
+        var event Event
+        _ = json.Unmarshal(msg, &event)
+        switch event.Type {
 			case "message_create": {
 				println("event message_create:" + string(msg))
 				_ = wss.BroadcastOthers(msg, s)
 				break
 			}
-		}
+        }
     })
 
     _ = server.Run(":9000")
