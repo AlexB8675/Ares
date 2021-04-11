6662e8d9b37150f3e227683bb4c777800fd716c1
AlexB8675 <alexandr.benbaccar@gmail.com>
2021-04-11 16:24:00 +0200

    formatting and types

--- php/delete.php
+++ php/delete.php
@@ -1,6 +1,6 @@
 <?php
     include_once 'common.php';
-    function leave_guild(mysqli $connection, int $guild, int $user) {
+    function leave_guild(mysqli $connection, int $guild, int $user): void {
         $query = "delete from UserGuild where guild_id = ? and user_id = ?";
         if (!safe_query($connection, $query, $guild, $user)) {
             die('query_error');
@@ -10,12 +10,7 @@
     start_session();
     $connection = connect_database();
     switch ($_POST['kind']) {
-        case 'guild': {
-            leave_guild($connection, intval($_POST['id']), $_SESSION['id']);
-        } break;
-
-        default: {
-            die('unknown_kind');
-        }
+        case 'guild': leave_guild($connection, intval($_POST['id']), $_SESSION['id']); break;
+        default:      die('unknown_kind');
     }
     mysqli_close($connection);
--- php/insert.php
+++ php/insert.php
@@ -1,6 +1,6 @@
 <?php
     include_once 'common.php';
-    function insert_avatar(mysqli $connection, int $user, string $path) {
+    function insert_avatar(mysqli $connection, int $user, string $path): void {
         $image = @imagecreatefromstring(file_get_contents($path));
         if (!$image) {
             die('unsupported_format');
@@ -19,7 +19,7 @@
         safe_query($connection, $query, "$target/avatar.png", $user);
     }
 
-    function insert_guild(mysqli $connection, int $id, int $user, string $name) {
+    function insert_guild(mysqli $connection, int $id, int $user, string $name): void {
         $query = "insert into Guild value (?, ?, null)"; // TODO: Maybe insert a Guild icon at creation-time?
         safe_query($connection, $query, $id, $name);
         $query = "insert into UserGuild value (?, ?)";
@@ -28,7 +28,7 @@
         safe_query($connection, $query, $id + 1, $id, "default");
     }
 
-    function insert_message(mysqli $connection, string $message) {
+    function insert_message(mysqli $connection, string $message): void {
         $data    = json_decode($message, true);
         $query   = "insert into Message value (?, ?, ?, ?)";
         $id      = intval($data['message']['id']);
@@ -38,7 +38,7 @@
         safe_query($connection, $query, $id, $user, $channel, $content);
     }
 
-    function join_guild(mysqli $connection, int $id, int $user) {
+    function join_guild(mysqli $connection, int $id, int $user): void {
         $query = "select name from Guild where id = ?";
         $guild = safe_query($connection, $query, $id)->get_result();
         if ($guild->num_rows === 0) {
@@ -56,24 +56,10 @@
     start_session();
     $connection = connect_database();
     switch ($_POST['kind']) {
-        case 'avatar': {
-            insert_avatar($connection, $_SESSION['id'], $_FILES['avatar']['tmp_name']);
-        } break;
-
-        case 'guild': {
-            insert_guild($connection, intval($_POST['id']), $_SESSION['id'], $_POST['name']);
-        } break;
-
-        case 'message': {
-            insert_message($connection, $_POST['message']);
-        } break;
-
-        case 'join': {
-            join_guild($connection, intval($_POST['id']), $_SESSION['id']);
-        } break;
-
-        default: {
-            die('unknown_kind');
-        }
+        case 'avatar':  insert_avatar($connection, $_SESSION['id'], $_FILES['avatar']['tmp_name']); break;
+        case 'guild':   insert_guild($connection, intval($_POST['id']), $_SESSION['id'], $_POST['name']); break;
+        case 'message': insert_message($connection, $_POST['message']); break;
+        case 'join':    join_guild($connection, intval($_POST['id']), $_SESSION['id']); break;
+        default:        die('unknown_kind');
     }
     mysqli_close($connection);
